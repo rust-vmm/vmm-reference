0879ab9a0caab2054997490eb2085db16488020c
Alexandra Iordache <aghecen@amazon.com>
2020-09-08 11:47:16 +0300

    Add more unit tests and update coverage
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- src/vmm/src/config.rs
+++ src/vmm/src/config.rs
@@ -2,18 +2,33 @@
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
 use std::convert::TryFrom;
+use std::fmt;
 use std::path::PathBuf;
 use std::result;
 
 /// Errors encountered converting the `*Config` objects.
-#[derive(Debug)]
 pub enum ConversionError {
-    /// Failed to parse a string representation.
-    Parse(String),
+    /// Failed to parse the string representation for the kernel.
+    ParseKernel(String),
+    /// Failed to parse the string representation for guest memory.
+    ParseMemory(String),
+    /// Failed to parse the string representation for the vCPUs.
+    ParseVcpus(String),
+}
+
+impl fmt::Display for ConversionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use ConversionError::*;
+        match self {
+            ParseKernel(ref s) => write!(f, "Invalid input for kernel: {}", s),
+            ParseMemory(ref s) => write!(f, "Invalid input for memory: {}", s),
+            ParseVcpus(ref s) => write!(f, "Invalid input for vCPUs: {}", s),
+        }
+    }
 }
 
 /// Guest memory configurations.
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct MemoryConfig {
     /// Guest memory size in MiB.
     pub mem_size_mib: u32,
@@ -25,22 +40,25 @@ impl TryFrom<String> for MemoryConfig {
     fn try_from(mem_cfg_str: String) -> result::Result<Self, Self::Error> {
         // Supported options: `size=<u32>`
         let mem_cfg_str_lower = mem_cfg_str.to_lowercase();
-        let tokens: Vec<&str> = mem_cfg_str_lower.split('=').collect();
+        let tokens: Vec<&str> = mem_cfg_str_lower
+            .split('=')
+            .filter(|tok| !tok.is_empty())
+            .collect();
         if tokens.len() != 2 {
-            return Err(ConversionError::Parse(mem_cfg_str));
+            return Err(ConversionError::ParseMemory(mem_cfg_str));
         }
         if tokens[0] != "mem_size_mib" {
-            return Err(ConversionError::Parse(tokens[0].to_string()));
+            return Err(ConversionError::ParseMemory(tokens[0].to_string()));
         }
         tokens[1]
             .parse::<u32>()
             .and_then(|mem_size_mib| Ok(MemoryConfig { mem_size_mib }))
-            .map_err(|_| ConversionError::Parse(tokens[1].to_string()))
+            .map_err(|_| ConversionError::ParseMemory(tokens[1].to_string()))
     }
 }
 
 /// vCPU configurations.
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct VcpuConfig {
     /// Number of vCPUs.
     pub num_vcpus: u8,
@@ -52,22 +70,25 @@ impl TryFrom<String> for VcpuConfig {
     fn try_from(vcpu_cfg_str: String) -> result::Result<Self, Self::Error> {
         // Supported options: `num_vcpus=<u8>`
         let vcpu_cfg_str_lower = vcpu_cfg_str.to_lowercase();
-        let tokens: Vec<&str> = vcpu_cfg_str_lower.split('=').collect();
+        let tokens: Vec<&str> = vcpu_cfg_str_lower
+            .split('=')
+            .filter(|tok| !tok.is_empty())
+            .collect();
         if tokens.len() != 2 {
-            return Err(ConversionError::Parse(vcpu_cfg_str));
+            return Err(ConversionError::ParseVcpus(vcpu_cfg_str));
         }
         if tokens[0] != "num_vcpus" {
-            return Err(ConversionError::Parse(tokens[0].to_string()));
+            return Err(ConversionError::ParseVcpus(tokens[0].to_string()));
         }
         tokens[1]
             .parse::<u8>()
             .and_then(|num_vcpus| Ok(VcpuConfig { num_vcpus }))
-            .map_err(|_| ConversionError::Parse(tokens[1].to_string()))
+            .map_err(|_| ConversionError::ParseVcpus(tokens[1].to_string()))
     }
 }
 
 /// Guest kernel configurations.
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct KernelConfig {
     /// Kernel command line.
     pub cmdline: String,
@@ -86,7 +107,7 @@ impl TryFrom<String> for KernelConfig {
         // Required: all
         let options: Vec<&str> = kernel_cfg_str.split(',').collect();
         if options.len() != 3 {
-            return Err(ConversionError::Parse(kernel_cfg_str));
+            return Err(ConversionError::ParseKernel(kernel_cfg_str));
         }
 
         let mut cmdline: Option<String> = None;
@@ -94,40 +115,41 @@ impl TryFrom<String> for KernelConfig {
         let mut himem_start: Option<u64> = None;
 
         for opt in options {
-            let tokens: Vec<&str> = opt.split('=').collect();
+            let tokens: Vec<&str> = opt.split('=').filter(|tok| !tok.is_empty()).collect();
             match tokens[0] {
                 "cmdline" => cmdline = Some(tokens[1..].join("=")),
                 "path" => {
                     if tokens.len() != 2 {
-                        return Err(ConversionError::Parse(opt.to_string()));
+                        return Err(ConversionError::ParseKernel(opt.to_string()));
                     }
                     path = Some(PathBuf::from(tokens[1]));
                 }
                 "himem_start" => {
                     if tokens.len() != 2 {
-                        return Err(ConversionError::Parse(opt.to_string()));
+                        return Err(ConversionError::ParseKernel(opt.to_string()));
                     }
                     himem_start = Some(
                         tokens[1]
                             .parse::<u64>()
-                            .map_err(|_| ConversionError::Parse(tokens[1].to_string()))?,
+                            .map_err(|_| ConversionError::ParseKernel(tokens[1].to_string()))?,
                     );
                 }
-                _ => return Err(ConversionError::Parse(kernel_cfg_str.to_string())),
+                _ => return Err(ConversionError::ParseKernel(kernel_cfg_str.to_string())),
             }
         }
 
         Ok(KernelConfig {
-            cmdline: cmdline.ok_or_else(|| ConversionError::Parse(kernel_cfg_str.to_string()))?,
-            path: path.ok_or_else(|| ConversionError::Parse(kernel_cfg_str.to_string()))?,
+            cmdline: cmdline
+                .ok_or_else(|| ConversionError::ParseKernel(kernel_cfg_str.to_string()))?,
+            path: path.ok_or_else(|| ConversionError::ParseKernel(kernel_cfg_str.to_string()))?,
             himem_start: himem_start
-                .ok_or_else(|| ConversionError::Parse(kernel_cfg_str.to_string()))?,
+                .ok_or_else(|| ConversionError::ParseKernel(kernel_cfg_str.to_string()))?,
         })
     }
 }
 
 /// VMM configuration.
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct VMMConfig {
     /// Guest memory configuration.
     pub memory_config: MemoryConfig,
