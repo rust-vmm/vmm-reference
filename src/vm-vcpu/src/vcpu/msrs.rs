24d20ca8d217a65aff4c6d531262c54084f35f1f
Andreea Florescu <fandree@amazon.com>
2021-03-03 12:55:50 +0200

    vm-vcpu: refactor create_boot_msr_entries
    Return Msrs object instead of a vector of kvm entries. This allows us to
    keep in one place the code that creates a static vector with MSRS, and
    the code that converts that vector to a FAM struct (`Msrs`). This is
    important because we can take the simplifying assumption that the
    entries have a valid size, and hence we can safely use unwrap.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- src/vm-vcpu/src/vcpu/msrs.rs
+++ src/vm-vcpu/src/vcpu/msrs.rs
@@ -3,24 +3,25 @@
 
 use std::default::Default;
 
-use kvm_bindings::kvm_msr_entry;
+use kvm_bindings::{kvm_msr_entry, Msrs};
 
 use crate::vcpu::msr_index::{
     MSR_CSTAR, MSR_IA32_MISC_ENABLE, MSR_IA32_MISC_ENABLE_FAST_STRING, MSR_IA32_SYSENTER_CS,
     MSR_IA32_SYSENTER_EIP, MSR_IA32_SYSENTER_ESP, MSR_IA32_TSC, MSR_KERNEL_GS_BASE, MSR_LSTAR,
     MSR_STAR, MSR_SYSCALL_MASK,
 };
+use crate::vcpu::{Error, Result};
 
 // Creates and populates required MSR entries for booting Linux on X86_64.
 // This should be offloaded to linux-loader.
-pub(crate) fn create_boot_msr_entries() -> Vec<kvm_msr_entry> {
+pub(crate) fn create_boot_msr_entries() -> Result<Msrs> {
     let msr_entry_default = |msr| kvm_msr_entry {
         index: msr,
         data: 0x0,
         ..Default::default()
     };
 
-    vec![
+    let raw_msrs = vec![
         msr_entry_default(MSR_IA32_SYSENTER_CS),
         msr_entry_default(MSR_IA32_SYSENTER_ESP),
         msr_entry_default(MSR_IA32_SYSENTER_EIP),
@@ -37,5 +38,7 @@ pub(crate) fn create_boot_msr_entries() -> Vec<kvm_msr_entry> {
             data: u64::from(MSR_IA32_MISC_ENABLE_FAST_STRING),
             ..Default::default()
         },
-    ]
+    ];
+
+    Msrs::from_entries(&raw_msrs).map_err(|_| Error::CreateMsrs)
 }
