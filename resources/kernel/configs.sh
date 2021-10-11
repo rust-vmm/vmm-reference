8279b46c302e18f085cc8481b5e4f6ba85ace53e
Sergii Glushchenko <gsserge@amazon.com>
2021-10-11 14:52:44 +0200

    Remove support for busybox 1.26
    Fixes: https://github.com/rust-vmm/vmm-reference/issues/159
    
    Signed-off-by: Sergii Glushchenko <gsserge@amazon.com>

--- resources/kernel/configs.sh
+++ resources/kernel/configs.sh
@@ -16,33 +16,8 @@ elif [[ $arch = "aarch64" ]]; then
 	KERNEL_CFG="microvm-kernel-initramfs-hello-aarch64.config"
 fi
 
-
-cat > glibc_ver.c << EOF
-#include <stdio.h>
-#include <gnu/libc-version.h>
-int main (void) { puts (gnu_get_libc_version ()); return 0; }
-
-EOF
-
-GLIBC_VER=`gcc -o glibc_ver glibc_ver.c 2>/dev/null && ./glibc_ver 2>/dev/null`
-
-#Delete the files generated
-rm -f glibc_ver* 2> /dev/null
-
-if [[ -z $GLIBC_VER ]]; then
-	echo "Unable to determine glibc version, using default Busybox: 1.26."
-	BUSYBOX_CFG="busybox_1_26_static_config"
-	BUSYBOX_VERSION="1.26.0"
-else
-	if [[ $GLIBC_VER < 2.31 ]]; then
-		BUSYBOX_CFG="busybox_1_26_static_config"
-		BUSYBOX_VERSION="1.26.0"
-	else
-		BUSYBOX_CFG="busybox_1_32_1_static_config"
-		BUSYBOX_VERSION="1.32.1"
-	fi
-
-fi
+BUSYBOX_CFG="busybox_1_32_1_static_config"
+BUSYBOX_VERSION="1.32.1"
 
 echo "Busybox Version: $BUSYBOX_VERSION"
 echo "Config: $BUSYBOX_CFG"
