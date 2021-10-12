d878c74ab6e8051237fa71626589ad3582b2a7a2
Sergii Glushchenko <gsserge@amazon.com>
2021-10-12 14:33:37 +0200

    Test resources

---
+++ .buildkite/build_resources.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+set -euo pipefail
+
+if git diff origin/main..HEAD --name-only | grep -q ^resources/ ; then
+  SKIP="false"
+else
+  SKIP="true"
+fi
+
+echo "
+steps:
+  - label: 'trigger-build-resources'
+    trigger: vmm-reference-build-resources
+    skip: ${SKIP}
+"
