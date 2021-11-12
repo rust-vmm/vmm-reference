9d57bb9fa3f6ab8721ac44ec036e577d70ce0175
Andreea Florescu <fandree@amazon.com>
2021-11-12 14:00:51 +0200

    lapic: return errors for invalid register off
    As we're going to export this as the public interface, we should not
    assume that callers will pass valid values. Instead of using expects, we
    should return errors.
    
    When using this in vm-vcpu for setting up the registers it is safe to
    use unwraps because we're using valid values for the register offset.
    Returning an error is making the code a bit uglier because the function
    where the lapic is set is returning only KvmError, so we would need to
    define another error just for the wrong register offset. This would make
    the function either return LapicError (the new defined enum variant) and
    KvmError.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- src/vm-vcpu-ref/src/x86_64/interrupts.rs
+++ src/vm-vcpu-ref/src/x86_64/interrupts.rs
@@ -43,23 +43,38 @@ pub const APIC_LVT1: usize = 0x360;
 pub const APIC_MODE_NMI: u32 = 0x4;
 pub const APIC_MODE_EXTINT: u32 = 0x7;
 
-pub fn get_klapic_reg(klapic: &kvm_lapic_state, reg_offset: usize) -> u32 {
-    let range = reg_offset..reg_offset + 4;
-    let reg = klapic.regs.get(range).expect("get_klapic_reg range");
-    read_le_i32(&reg) as u32
+/// Errors associated with operations related to interrupts.
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    /// The register offset is invalid.
+    InvalidRegisterOffset,
 }
 
-pub fn set_klapic_reg(klapic: &mut kvm_lapic_state, reg_offset: usize, value: u32) {
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub fn get_klapic_reg(klapic: &kvm_lapic_state, reg_offset: usize) -> Result<u32> {
     let range = reg_offset..reg_offset + 4;
-    let mut reg = klapic.regs.get_mut(range).expect("set_klapic_reg range");
-    write_le_i32(&mut reg, value as i32)
+    let reg = klapic.regs.get(range).ok_or(Error::InvalidRegisterOffset)?;
+    Ok(read_le_i32(&reg) as u32)
+}
+
+pub fn set_klapic_reg(klapic: &mut kvm_lapic_state, reg_offset: usize, value: u32) -> Result<()> {
+    // The value that we are setting is a u32, which needs 4 bytes of space.
+    // We're here creating a range that can fit the whole value.
+    let range = reg_offset..reg_offset + 4;
+    let mut reg = klapic
+        .regs
+        .get_mut(range)
+        .ok_or(Error::InvalidRegisterOffset)?;
+    write_le_i32(&mut reg, value as i32);
+    Ok(())
 }
 
 fn set_apic_delivery_mode(reg: u32, mode: u32) -> u32 {
     ((reg) & !0x700) | ((mode) << 8)
 }
 
-/// Set the APIC delivery mode.
+/// Set the APIC delivery mode. Returns an error when the register configuration is invalid.
 ///
 /// # Arguments
 /// * `klapic`: The corresponding `kvm_lapic_state` for which to set the delivery mode.
@@ -68,7 +83,41 @@ fn set_apic_delivery_mode(reg: u32, mode: u32) -> u32 {
 /// * `mode`: The APIC mode to set. Available options are:
 ///           [Non Maskable Interrupt - NMI](APIC_MODE_NMI) and
 ///           [external interrupt - ExtINT](APIC_MODE_EXTINT).
-pub fn set_klapic_delivery_mode(klapic: &mut kvm_lapic_state, reg_offset: usize, mode: u32) {
-    let reg_value = get_klapic_reg(&klapic, reg_offset);
-    set_klapic_reg(klapic, reg_offset, set_apic_delivery_mode(reg_value, mode));
+pub fn set_klapic_delivery_mode(
+    klapic: &mut kvm_lapic_state,
+    reg_offset: usize,
+    mode: u32,
+) -> Result<()> {
+    let reg_value = get_klapic_reg(&klapic, reg_offset)?;
+    set_klapic_reg(klapic, reg_offset, set_apic_delivery_mode(reg_value, mode))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::x86_64::interrupts::{
+        get_klapic_reg, set_klapic_delivery_mode, Error, APIC_MODE_EXTINT,
+    };
+    use kvm_bindings::kvm_lapic_state;
+
+    #[test]
+    fn test_reg_offset() {
+        // The size of `regs` in `kvm_lapic_state` is 1024. Since we're setting a value of
+        // 4 bytes, if we want to set it at offset = 1020 it should fit.
+        let offset = 1020;
+        let mut klapic = kvm_lapic_state::default();
+        assert!(set_klapic_delivery_mode(&mut klapic, offset, APIC_MODE_EXTINT).is_ok());
+        assert!(get_klapic_reg(&klapic, offset).is_ok());
+
+        // Setting at the offset og 1021 does not work because 4 bytes don't fit.
+        let offset = 1021;
+        let mut klapic = kvm_lapic_state::default();
+        assert_eq!(
+            set_klapic_delivery_mode(&mut klapic, offset, APIC_MODE_EXTINT).unwrap_err(),
+            Error::InvalidRegisterOffset
+        );
+        assert_eq!(
+            get_klapic_reg(&klapic, offset).unwrap_err(),
+            Error::InvalidRegisterOffset
+        );
+    }
 }
